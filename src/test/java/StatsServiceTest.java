c119cb431584bdcc08e39e62db7fa7bfc04ab87f
Nikita Kholopov <vcarkills@gmail.com>
2023-06-21 17:50:26 +0300

    dz6

---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,56 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+
+
+    @Test
+    public void testSum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.average(sales);
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testMinSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.minSales(sales);
+        long expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testMaxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.maxSales(sales);
+        long expected = 8;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+}
